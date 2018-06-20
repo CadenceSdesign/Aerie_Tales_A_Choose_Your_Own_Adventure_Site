e92c37f4dcfb45164db7fd815c904bb00723a856
CadenceSdesign <35323943+CadenceSdesign@users.noreply.github.com>
2018-06-20 17:54:35 -0400

    Added Story Results divs
    Added Story Result divs after each mini-story to both show the results of the user’s choice and to add a pause before the beginning of the next story. Adjusted functions to hide choice buttons that are already used and/or no longer needed. Adjusted functions using “innerHTML” to jQuery “.text”.

--- js/choiceButtons.js
+++ js/choiceButtons.js
@@ -4,33 +4,56 @@ var excited= 0;
 
 
 function Story1Choice1() {
-    document.getElementById('btnStory1Choice2').style.visibility = 'hidden';
-    calm += 1;
-    document.getElementById('story2').style.display = 'block';
-    document.getElementById('navStory2').style.display = 'block';
+   calm += 1;
+   $("#story1ResultTitle").text("You put the egg by the fire.");
+   $("#story1ResultText").text("Now it's warm and calm.");
+   NavStory1Result();
 }
 
 function Story1Choice2() {
-    document.getElementById('btnStory1Choice1').style.visibility = 'hidden';
-    excited += 1;
-    document.getElementById('story2').style.display = 'block';
-    document.getElementById('navStory2').style.display = 'block';
+   excited += 1; 
+   $("#story1ResultTitle").text("You put the egg by the window.");
+   $("#story1ResultText").text("The wind and snow inspire and excite it.");
+   NavStory1Result();
+}
+
+function NavStory1Result() {
+   document.getElementById('btnStory1Choice1').style.display = 'none';
+   document.getElementById('btnStory1Choice2').style.display = 'none';
+   document.getElementById('story1Result').style.display = 'block';
+}
+
+function ContinueToStory2(){
+   document.getElementById('btnContinueToStory2').style.display = 'none';
+   document.getElementById('story2').style.display = 'block';
+   document.getElementById('navStory2').style.display = 'block';
 }
 
 function Story2Choice1() {
-    document.getElementById('btnStory2Choice2').style.visibility = 'hidden';
-    calm += 1;
-    generateEnding();
-    document.getElementById('storyEnd').style.display = 'block';
-    document.getElementById('navStoryEnd').style.display = 'block';
+   calm += 1;
+   $("#story2ResultTitle").text("You brought the egg to the libray.");
+   $("#story2ResultText").text("The quiet made it a bit calmer.");
+   NavStory2Result();
 }
 
 function Story2Choice2() {
-    document.getElementById('btnStory2Choice1').style.visibility = 'hidden';
     excited += 1;
-    generateEnding();
-    document.getElementById('storyEnd').style.display = 'block';
-    document.getElementById('navStoryEnd').style.display = 'block';   
+   $("#story2ResultTitle").text("You brought the egg to the show.");
+   $("#story2ResultText").text("The air routines got it very excited.");
+   NavStory2Result();
+}
+
+function NavStory2Result() {
+   document.getElementById('btnStory2Choice1').style.display = 'none';
+   document.getElementById('btnStory2Choice2').style.display = 'none';
+   document.getElementById('story2Result').style.display = 'block';
+}
+
+function ContinueToStoryEnd(){
+   generateEnding();
+   document.getElementById('btnContinueToStoryEnd').style.display = 'none';
+   document.getElementById('storyEnd').style.display = 'block';
+   document.getElementById('navStoryEnd').style.display = 'block';
 }
 
 function generateEnding(){
@@ -52,5 +75,5 @@ function generateEnding(){
   }
   
   
-  document.getElementById("endingText").innerHTML = ending;
+  $("#endingText").text(ending);
 }
